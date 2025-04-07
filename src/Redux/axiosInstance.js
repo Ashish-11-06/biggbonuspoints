ead73d035651f6d96f9a176dfaa9954264cace5a
Ashish Bhosale <bhosaleashish311@gmail.com>
2025-04-07 15:27:46 +0530

    added

--- src/Redux/axiosInstance.js
+++ src/Redux/axiosInstance.js
@@ -2,7 +2,7 @@ import { create } from 'apisauce';
 import Reactotron from 'reactotron-react-native';
 
 const axiosInstance = create({
-  baseURL: 'c234-103-211-60-165.ngrok-free.app/api/',
+  baseURL: 'https://c234-103-211-60-165.ngrok-free.app/api/',
   headers: {
     'Content-Type': 'application/json',
 },
